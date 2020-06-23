8168be37c3b7e40903336c7850ef16b3fb6f8c87
Mayank khattar <mayankkhattar007@gmail.com>
2020-06-23 20:45:04 +0530

    Built the UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" class="appbg">
+      <div class="equation">Equation</div>
+      <div class="answer">Answer</div>
+      <div class="grid-container">
+        <div class="grid-item">1</div>
+        <div class="grid-item">2</div>
+        <div class="grid-item">3</div>
+        <div class="grid-item">/</div>
+        <div class="grid-item">4</div>
+        <div class="grid-item">5</div>
+        <div class="grid-item">6</div>
+        <div class="grid-item">*</div>  
+        <div class="grid-item">7</div>
+        <div class="grid-item">8</div>
+        <div class="grid-item">9</div>
+        <div class="grid-item">-</div>  
+        <div class="grid-item">C</div>
+        <div class="grid-item">0</div>
+        <div class="grid-item">=</div>
+        <div class="grid-item">+</div>
+      </div>
     </div>
   );
 }
