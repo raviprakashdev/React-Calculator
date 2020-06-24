4a5567e0542ddc71625365791c2a8e69a09ddbbc
Mayank khattar <mayankkhattar007@gmail.com>
2020-06-24 15:50:37 +0530

    Backend now working

--- src/App.js
+++ src/App.js
@@ -1,31 +1,73 @@
 import React from 'react';
+import { evaluate } from 'mathjs'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App" class="appbg">
-      <div class="equation">Equation</div>
-      <div class="answer">Answer</div>
-      <div class="grid-container">
-        <div class="grid-item">1</div>
-        <div class="grid-item">2</div>
-        <div class="grid-item">3</div>
-        <div class="grid-item">/</div>
-        <div class="grid-item">4</div>
-        <div class="grid-item">5</div>
-        <div class="grid-item">6</div>
-        <div class="grid-item">*</div>  
-        <div class="grid-item">7</div>
-        <div class="grid-item">8</div>
-        <div class="grid-item">9</div>
-        <div class="grid-item">-</div>  
-        <div class="grid-item">C</div>
-        <div class="grid-item">0</div>
-        <div class="grid-item">=</div>
-        <div class="grid-item">+</div>
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      equation:"Equation",
+      answer:"Answer"
+    };
+  }
+
+  addtoParseStack(e){
+    let mystr = this.state.equation
+    if (mystr==="Equation"){
+      mystr = ""
+    }
+    mystr = mystr + e
+    let ans;
+    try {
+      ans = evaluate(mystr)
+    } catch (error) {
+      ans = error.message
+    }
+    this.setState({
+      equation:mystr,
+      answer: ans
+    })
+  }
+
+  clearStack(){
+    this.setState({
+      equation:"Equation",
+      answer:"Answer"
+    })
+  }
+  answerEqSwitch(){
+    this.setState({
+      equation:this.state.answer,
+      answer:"Answer"
+    })
+  }
+  render(){
+    return (
+      <div className="App">
+        <div class="equation">{this.state.equation}</div>
+        <div class="answer">{this.state.answer}</div>
+        <div class="grid-container">
+          <div class="grid-item" onClick={()=> this.addtoParseStack("1")}>1</div>
+          <div class="grid-item" onClick={()=> this.addtoParseStack("2")}>2</div>
+          <div class="grid-item" onClick={()=> this.addtoParseStack("3")}>3</div>
+          <div class="grid-item" onClick={()=> this.addtoParseStack("/")}>/</div>
+          <div class="grid-item" onClick={()=> this.addtoParseStack("4")}>4</div>
+          <div class="grid-item" onClick={()=> this.addtoParseStack("5")}>5</div>
+          <div class="grid-item" onClick={()=> this.addtoParseStack("6")}>6</div>
+          <div class="grid-item" onClick={()=> this.addtoParseStack("*")}>*</div>  
+          <div class="grid-item" onClick={()=> this.addtoParseStack("7")}>7</div>
+          <div class="grid-item" onClick={()=> this.addtoParseStack("8")}>8</div>
+          <div class="grid-item" onClick={()=> this.addtoParseStack("9")}>9</div>
+          <div class="grid-item" onClick={()=> this.addtoParseStack("-")}>-</div>  
+          <div class="grid-item" onClick={()=> this.clearStack()}>C</div>
+          <div class="grid-item" onClick={()=> this.addtoParseStack("0")}>0</div>
+          <div class="grid-item" onClick={()=> this.answerEqSwitch()}>=</div>
+          <div class="grid-item" onClick={()=> this.addtoParseStack("+")}>+</div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
